1e61c1cf82aaff3e825bc992cf5ff8e19408f25d
isavolainen <35977587+isavolainen@users.noreply.github.com>
2021-12-29 13:58:56 +0200

    Add files via upload

--- matopeli.py
+++ matopeli.py
@@ -1,323 +1,253 @@
 #-------------------------------------------------------------------------------
 # Module       matopeli
 # Author:      Ilja Savolainen
-# Created:     01.02.2018
+# Created:     29.12.2021
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
-import pygame
 import random
-import time
+import pygame
 
-SCREEN_X = 800
-SCREEN_Y = 800
+# Settings
+TITLE = "Snape"
+VERSION = 1.0
+DIFFICULTY = 8
+FONT = "Consolas"
+(SCREEN_X, SCREEN_Y) = (800, 800)
 FPS = 60
 MARGIN = 20
 BORDER = 10
-BG_COLOR = (100,20,0)
-COLOR = (255,255,255)
-BORDER_COLOR = COLOR
-TEXT_COLOR = COLOR
-SNAKE_COLOR = COLOR
-FOOD_COLOR = COLOR
-
-SPEED = 16
-SNAKE_SIZE = 16
-FOOD_SIZE = SNAKE_SIZE
-(UP, RIGHT, DOWN, LEFT) = (0, 1, 2, 3)
-
-KEY_ESC = 27
-KEY_N = 110
-KEY_T = 116
-KEY_Y = 121
-KEY_UP = 273
-KEY_DOWN = 274
-KEY_RIGHT = 275
-KEY_LEFT = 276
-
-arrow_keys = (KEY_UP, KEY_DOWN, KEY_RIGHT, KEY_LEFT)
-perimeter = MARGIN + BORDER
-mid = (SCREEN_X//2, SCREEN_Y//2)
-#grid = (SCREEN_X//FOOD_SIZE, SCREEN_Y//FOOD_SIZE)
-
+EDGE = MARGIN + BORDER
+COLOR = (255, 180, 180)
+BG_COLOR = (100, 20, 0)
 
 # Class definitions
 class Snake:
 
-    heading = RIGHT
-    speed = SPEED
-    color = SNAKE_COLOR
-    length = 20
-
-
-    def __init__(self, size=SNAKE_SIZE, pos=mid):
-        """ Create and initialize a Snake instance of target size at
-            target position.
-        """
-        self.size = size
+    def __init__(self, pos=(SCREEN_X//2, SCREEN_Y//2)):
+        """Create and initialize a Snake instance at target position."""
+        self.heading = "right"
+        self.speed = 4
+        self.length = 32
+        self.size = 16
+        self.color = COLOR
         self.pos = pos
-        print(self.pos)
-        self.x = []
-        self.y = []
-        for i in range(self.length):
-            self.x.append(self.pos[0])
-            self.y.append(self.pos[1])
-        #print(self.x)
-        #print(self.y)
+        (self.x_coord, self.y_coord) = ([], [])
+        self.displacement = 0
+        for _ in range(self.length):
+            self.x_coord.append(self.pos[0] - self.displacement)
+            self.y_coord.append(self.pos[1])
+            self.displacement += 4
 
-    def update(self):
-        """ Update the position of all sections. """
-        if self.heading == UP:
-            self.y[0] -= self.speed
-        elif self.heading == RIGHT:
-            self.x[0] += self.speed
-        elif self.heading == DOWN:
-            self.y[0] += self.speed
-        elif self.heading == LEFT:
-            self.x[0] -= self.speed
-        #print(self.x[0], self.y[0])
+    def turn(self, direction):
+        if direction == "right" and self.heading != "left":
+            self.heading = "right"
+        elif direction == "left" and self.heading != "right":
+            self.heading = "left"
+        elif direction == "up" and self.heading != "down":
+            self.heading = "up"
+        elif direction == "down" and self.heading != "up":
+            self.heading = "down"
 
-        for i in range(self.length-1, 0, -1):
-            self.x[i] = self.x[i-1]
-            self.y[i] = self.y[i-1]
-
-    def turn(self, key):
-        if key == KEY_UP and self.heading != DOWN:
-            self.heading = UP
-        elif key == KEY_RIGHT and self.heading != LEFT:
-            self.heading = RIGHT
-        elif key == KEY_DOWN and self.heading != UP:
-            self.heading = DOWN
-        elif key == KEY_LEFT and self.heading != RIGHT:
-            self.heading = LEFT
+    def move(self):
+        # Moving the head
+        if self.heading == "right":
+            self.x_coord[0] += self.speed
+        elif self.heading == "left":
+            self.x_coord[0] -= self.speed
+        elif self.heading == "up":
+            self.y_coord[0] -= self.speed
+        elif self.heading == "down":
+            self.y_coord[0] += self.speed
+        # Moving the rest
+        for i in range(self.length - 1, 0, -1):
+            self.x_coord[i] = self.x_coord[i - 1]
+            self.y_coord[i] = self.y_coord[i - 1]
 
     def grow(self):
-        self.length += 1
-        self.x.append(self.x[-1])
-        self.y.append(self.y[-1])
+        for _ in range(DIFFICULTY):
+            self.length += 1
+            self.x_coord.append(self.x_coord[-1])
+            self.y_coord.append(self.y_coord[-1])
 
     def draw(self, surface):
         for i in range(self.length):
-            draw_area = (self.x[i], self.y[i], self.size, self.size)
+            draw_area = (self.x_coord[i], self.y_coord[i], self.size, self.size)
             surface.fill(self.color, draw_area)
 
-##    def collision(self): # Bounce from walls
-##        (x, y) = self.pos
-##        if x <= perimeter:
-##            self.pos = (x+1, y)
-##            self.vx = -self.vx
-##        if x >= SCREEN_X-self.size-perimeter:
-##            self.pos = (x-1, y)
-##            self.vx = -self.vx
-##        if y < perimeter:
-##            self.pos = (x, y+1)
-##            self.vy = -self.vy
-##        if y >= SCREEN_Y-self.size-perimeter:
-##            self.pos = (x, y-1)
-##            self.vy = -self.vy
-
     def collision(self):
+        """Checks if the snake has collided with a wall or with itself."""
         # Check collision with walls
-        (x, y) = (self.x[0], self.y[0])
-        if x <= perimeter or x >= SCREEN_X-self.size-perimeter or \
-           y <= perimeter or y >= SCREEN_Y-self.size-perimeter:
+        (x_coord, y_coord) = (self.x_coord[0], self.y_coord[0])
+        if x_coord <= EDGE or x_coord >= SCREEN_X - self.size - EDGE or \
+            y_coord <= EDGE or y_coord >= SCREEN_Y - self.size - EDGE:
             return True
-
         # Check collision with self
-
+        corners = self.get_corners()
+        if self.heading == "right":
+            (frontleft_x, frontleft_y) = (corners[1][0], corners[1][1])
+            (frontright_x, frontright_y) = (corners[2][0], corners[2][1])
+        elif self.heading == "left":
+            (frontleft_x, frontleft_y) = (corners[3][0], corners[3][1])
+            (frontright_x, frontright_y) = (corners[0][0], corners[0][1])
+        elif self.heading == "up":
+            (frontleft_x, frontleft_y) = (corners[0][0], corners[0][1])
+            (frontright_x, frontright_y) = (corners[1][0], corners[1][1])
+        elif self.heading == "down":
+            (frontleft_x, frontleft_y) = (corners[2][0], corners[2][1])
+            (frontright_x, frontright_y) = (corners[3][0], corners[3][1])
+        for i in range(len(self.x_coord)):
+            if self.x_coord[i] < frontleft_x < self.x_coord[i] + self.size and \
+                self.y_coord[i] < frontleft_y < self.y_coord[i] + self.size:
+                return True
+            if self.x_coord[i] < frontright_x < self.x_coord[i] + self.size and \
+                self.y_coord[i] < frontright_y < self.y_coord[i] + self.size:
+                return True
         return False
 
     def get_corners(self):
-        (x, y) = (self.x[0], self.y[0])
-        a = (x, y)
-        b = (x+self.size, y)
-        c = (x+self.size, y+self.size)
-        d = (x, y+self.size)
-        return (a, b, c, d)
-
+        """Returns the coordinates for all corners of the snake's head."""
+        (x_coord, y_coord) = (self.x_coord[0], self.y_coord[0])
+        corner0 = (x_coord, y_coord)
+        corner1 = (x_coord + self.size, y_coord)
+        corner2 = (x_coord + self.size, y_coord + self.size)
+        corner3 = (x_coord, y_coord + self.size)
+        return (corner0, corner1, corner2, corner3)
 
 class Food:
-
-    def __init__(self, size=FOOD_SIZE, color=FOOD_COLOR):
-        """ Create and initialize a Food object at a random position. """
-        self.size = size
-        self.color = color
+    def __init__(self):
+        """Create and initialize a Food instance at a random location."""
+        self.size = 16
+        self.color = COLOR
         self.pos = self.spawn()
 
     def spawn(self):
-        """ Returns a random location (x, y) on the playing area. """
-        (x, y) = (0, 0)
+        """Spawn in a random location."""
+        (x_coord, y_coord) = (0, 0)
         grid_x = SCREEN_X // self.size
         grid_y = SCREEN_Y // self.size
-        while x < perimeter+5 or x > SCREEN_X-self.size-perimeter-5:
-            x = random.randrange(grid_x) * self.size
-        while y < perimeter+5 or y > SCREEN_Y-self.size-perimeter-5:
-            y = random.randrange(grid_y) * self.size
-        return (x, y)
+        while x_coord < EDGE + 5 or x_coord > SCREEN_X - self.size - EDGE - 5:
+            x_coord = random.randrange(grid_x) * self.size
+        while y_coord < EDGE + 5 or y_coord > SCREEN_Y - self.size - EDGE - 5:
+            y_coord = random.randrange(grid_y) * self.size
+        return (x_coord, y_coord)
 
     def draw(self, surface):
-        (x, y) = self.pos
-        draw_area = (x, y, self.size, self.size)
+        draw_area = (self.pos[0], self.pos[1], self.size, self.size)
         surface.fill(self.color, draw_area)
 
 
 # Function definitions
-def draw_border(surface):
-    (width, margin) = (BORDER, MARGIN)
-    length_x = SCREEN_X-2*margin
-    length_y = SCREEN_Y-2*margin
-    color = BORDER_COLOR
-    b_left = (margin, margin, width, length_y)
-    b_right = (SCREEN_X-perimeter, margin, width, length_y)
-    b_top = (margin, margin, length_x, width)
-    b_bottom = (margin, SCREEN_Y-perimeter, length_x, width)
-    surface.fill(color, b_left)
-    surface.fill(color, b_right)
-    surface.fill(color, b_top)
-    surface.fill(color, b_bottom)
-
-
 def draw_endscreen(surface):
     surface.fill(BG_COLOR)
-    game_over_1 = font_xl.render("YOU DIED", True, (TEXT_COLOR))
-    game_over_2 = font_l.render("Score: {0}".format(score),
-            True, (TEXT_COLOR))
-    game_over_3 = font_l.render("Do you want to play again? y/n",
-            True, (TEXT_COLOR))
-    rect_1 = game_over_1.get_rect(center=mid)
-    rect_1 = (rect_1[0], rect_1[1]-SCREEN_Y//8)
-    rect_2 = game_over_2.get_rect(center=mid)
+    game_over_1 = font_xl.render("YOU DIED", True, (COLOR))
+    game_over_2 = font_l.render(f"Score: {score}", True, (COLOR))
+    game_over_3 = font_l.render("Do you want to play again? y/n", True, (COLOR))
+    rect_1 = game_over_1.get_rect(center = (SCREEN_X // 2, SCREEN_Y // 2))
+    rect_1 = (rect_1[0], rect_1[1] - SCREEN_Y // 8)
+    rect_2 = game_over_2.get_rect(center = (SCREEN_X // 2, SCREEN_Y // 2))
     rect_2 = (rect_2[0], rect_2[1])
-    rect_3 = game_over_3.get_rect(center=mid)
-    rect_3 = (rect_3[0], rect_3[1]+SCREEN_Y//12)
+    rect_3 = game_over_3.get_rect(center = (SCREEN_X // 2, SCREEN_Y // 2))
+    rect_3 = (rect_3[0], rect_3[1] + SCREEN_Y // 12)
     surface.blit(game_over_1, rect_1)
     surface.blit(game_over_2, rect_2)
     surface.blit(game_over_3, rect_3)
 
-
-def collides(snake, food):
-    s_corners = snake.get_corners()
-    (f_x, f_y) = food.pos
-    for coord in s_corners:
-        (x, y) = coord
-        if f_x <= x <= f_x+food.size and f_y <= y <= f_y+food.size:
+def collides(snake_object, food_object):
+    """Checks if the snake has collided with food."""
+    snake_corners = snake_object.get_corners()
+    (food_x, food_y) = food_object.pos
+    for coord in snake_corners:
+        if food_x <= coord[0] <= food_x + food_object.size and \
+            food_y <= coord[1] <= food_y + food_object.size:
             return True
     return False
 
-# main():
-while True:
-    """ Main loop """
 
-    # Game setup
-    pygame.init()
-    clock = pygame.time.Clock()
-    surface = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
-    pygame.display.set_caption('Snape v0.1')
-    pygame.mixer.init(48000, -16, 2, 2048)
-    font_xl = pygame.font.SysFont("Consolas", 64)
-    font_l = pygame.font.SysFont("Consolas", 24)
-    font_m = pygame.font.SysFont("Consolas", 18)
-    font_s = pygame.font.SysFont("Consolas", 14)
-    try:
-        death_sound = pygame.mixer.Sound("you_died.ogg")
-        death_sound.set_volume(0.5)
-    except:
-        pass
-    snake = Snake()
-    food = Food()
-    score = 0
-    game_state = 1
+if __name__ == "__main__":
+    # Main loop
+    while True:
+        # Game setup
+        pygame.init()
+        clock = pygame.time.Clock()
+        screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
+        pygame.display.set_caption(f'{TITLE} {VERSION}')
+        font_xl = pygame.font.SysFont(FONT, 96)
+        font_l = pygame.font.SysFont(FONT, 24)
+        font_m = pygame.font.SysFont(FONT, 18)
+        snake = Snake()
+        food = Food()
+        score = 0
+        game_state = 1
+        frame_count = 0
+        seconds = 0
 
-    # FPS
-    frame_count = 0
-    frame_rate = 0
-    draw_stats = 0
-    t0 = time.clock()
+        # Game loop
+        while game_state == 1:
+            # Event handling
+            for event in pygame.event.get():
+#                if event.type == pygame.KEYDOWN:
+#                    print(event)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        snake.turn("left")
+                    if event.key == pygame.K_RIGHT:
+                        snake.turn("right")
+                    if event.key == pygame.K_UP:
+                        snake.turn("up")
+                    if event.key == pygame.K_DOWN:
+                        snake.turn("down")
+                    if event.key == pygame.K_ESCAPE:
+                        game_state = -1
+                        break
+                if event.type == pygame.QUIT:
+                    game_state = -1
+                    break
 
-    while game_state == 1:
-        """ Game loop """
+            # Gameplay
+            snake.move()
+            if collides(snake, food):
+                score += 10
+                snake.grow()
+                del food
+                food = Food()
+            if snake.collision():
+                game_state = 0
 
-        # Event handling
-        ev = pygame.event.poll()
-        if ev.type != pygame.NOEVENT and ev.type != pygame.MOUSEMOTION and\
-                ev.type != pygame.KEYUP:
-            print(ev)
-        if ev.type == pygame.QUIT:
-            game_state = -1
+            # Draw calls
+            screen.fill(BG_COLOR)
+            screen.fill(COLOR, (MARGIN, MARGIN, SCREEN_X - 2 * MARGIN, SCREEN_Y - 2 * MARGIN))
+            screen.fill(BG_COLOR, (EDGE, EDGE, SCREEN_X - 2 * EDGE, SCREEN_Y - 2 * EDGE))
+            the_score = font_l.render(f"Score: {score}", True, COLOR)
+            screen.blit(the_score, (EDGE + 10, EDGE + 10))
+            time = font_l.render(f"Time: {seconds}", True, COLOR)
+            screen.blit(time, (SCREEN_X - 150, EDGE + 10))
+            snake.draw(screen)
+            food.draw(screen)
+            pygame.display.flip()
+            clock.tick(FPS)
+            frame_count += 1
+            if frame_count % 60 == 0:
+                seconds += 1
+
+        # End loop
+        while game_state == 0:
+            # Event handling
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_state = -1
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE or event.key == pygame.K_n:
+                        game_state = -1
+                    if event.key == pygame.K_y:
+                        game_state = 1
+
+            # Draw calls
+            draw_endscreen(screen)
+            pygame.display.flip()
+            clock.tick(FPS)
+
+        if game_state < 1:
             break
-        if ev.type == pygame.KEYDOWN:
-            key = ev.dict["key"]
-            if key == KEY_ESC:
-                game_state = -1
-                break
-            if key == KEY_T:
-                if draw_stats == 0:
-                    draw_stats = 1
-                else:
-                    draw_stats = 0
-            if key in arrow_keys:
-                snake.turn(key)
 
-        # FPS
-        frame_count += 1
-        if frame_count % 30 == 0:
-            t1 = time.clock()
-            frame_rate = 30 / (t1-t0)
-            t0 = t1
-
-        if frame_count % 4 == 0:
-            snake.update()
-        if collides(snake, food):
-            score += 1
-            snake.grow()
-            del food
-            food = Food()
-
-        # Draw calls
-        surface.fill(BG_COLOR)
-        draw_border(surface)
-        the_score = font_m.render("Score: {0}"
-                .format(score), True, (255,255,255))
-        surface.blit(the_score, (perimeter+10, perimeter+10))
-        if draw_stats == 1:
-            frame_text = font_s.render("Frame: {0}"
-                    .format(frame_count), True, (TEXT_COLOR))
-            fps_text = font_s.render("FPS: {0:.2f}"
-                    .format(frame_rate), True, (TEXT_COLOR))
-            surface.blit(frame_text, (perimeter+10, SCREEN_Y-perimeter-50))
-            surface.blit(fps_text, (perimeter+10, SCREEN_Y-perimeter-30))
-        snake.draw(surface)
-        food.draw(surface)
-        pygame.display.flip()
-        clock.tick(FPS)
-        if snake.collision():
-            game_state = 0
-            #death_sound.play()
-
-    while game_state == 0:
-        """ End loop """
-
-        # Event handling
-        ev = pygame.event.poll()
-        if ev.type != pygame.NOEVENT and ev.type != pygame.MOUSEMOTION and \
-                ev.type != pygame.KEYUP:
-            print(ev)
-        if ev.type == pygame.QUIT:
-            break
-        if ev.type == pygame.KEYDOWN:
-            key = ev.dict["key"]
-            if key == KEY_ESC or key == KEY_N:
-                break
-            if key == KEY_Y:
-                game_state = 1
-                snake = Snake()
-
-        # Draw calls
-        draw_endscreen(surface)
-        pygame.display.flip()
-        clock.tick(FPS)
-
-    if game_state < 1:
-        break
-
-pygame.quit()
+    pygame.quit()
